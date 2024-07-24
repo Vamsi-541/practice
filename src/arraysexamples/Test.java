30d19ef49c2dd7a34874f1ec2dac627ef6bbf071
Vamsi-541 <krishnaaddankivamsi@gmail.com>
2024-07-24 12:55:53 +0530

    Updated file

--- src/arraysexamples/Test.java
+++ src/arraysexamples/Test.java
@@ -4,7 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ 
+		int a=10;
+		int b=30;
+		int c = a+b;
+		System.out.println(c + '=' + a+b);
+		
 	}
 
 }
+
